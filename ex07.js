19ac6fdc138635d82f59258d1c5ff2958f80dfdb
Gabriel Suldine <82013387+Gabriel-Suldine@users.noreply.github.com>
2022-06-20 09:23:41 -0400

    Delete

--- ex07.js
+++
@@ -1,8 +0,0 @@
-//Crie um algoritmo que percorra o vetor filmes e imprima apenas os filmes que contenham um subtítulo.
-import { filmes } from "./dados.js";
-
-for(let i = 0; i < filmes.length; i++){
-    if (filmes[i].includes(":")){
-        console.log(filmes[i])
-    }   
-}
